c48687072cbd3b8553869ce3cba9689da66003d3
Sinan NAR <sinan.nar@outlook.com>
2017-12-03 17:47:33 +1300

    Exception at event has been handled

--- DelegateExample/Program.cs
+++ DelegateExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EventsandCallbacks
 {
@@ -52,9 +53,35 @@ namespace EventsandCallbacks
             example9.RunExample();
             Console.WriteLine("==================");
 
+            Console.WriteLine("UsingEventHandler");
+            var example10 = new UsingEventHandler();
+            example10.RunExample();
+            Console.WriteLine("==================");
+
+            Console.WriteLine("UsingEventHandler2");
+            var example11 = new UsingEventHandler2();
+            example11.RunExample();
+            Console.WriteLine("==================");
+
+            Console.ReadKey();
+
+            Console.WriteLine("ExceptionAtEventHandlerExample");
+            var example12 = new ExceptionAtEventHandlerExample();
+            example12.RunExample();
+            Console.WriteLine("==================");
+
+
+            Console.ReadKey();
+
+            Console.WriteLine("ExceptionAtEventHandlerExample2");
+            var example13 = new ExceptionAtEventHandlerExample2();
+            example13.RunExample();
+            Console.WriteLine("==================");
 
 
 
+            Console.ReadKey();
+            Console.ReadKey();
             Console.ReadKey();
             Console.WriteLine();
         }
@@ -294,5 +321,178 @@ namespace EventsandCallbacks
     /*
         So we change Pub class to re implement as Pub2 with event with this 2 problem. Direc assignment is not valid also eventhought OnChange is public, outside of class they can not invoke it.
         Initialising OnChange with delegate{} will let me decrease the code because it is initialised so it can not be null anymore.
+        Instead of using Action type event, we can use EventHandler or EvenHandler<T> in this code
      */
+
+    public class MyArgs:EventArgs
+    {
+        public MyArgs(int value)
+        {
+            Value = value;
+        }
+        public int Value { get; set; }
+    }
+
+    public class Pub3
+    {
+        public event EventHandler<MyArgs> OnChange = delegate { };
+        public void Raise()
+        {
+            OnChange(this, new MyArgs(42));
+        }
+    }
+    class UsingEventHandler
+    {
+        public void RunExample()
+        {
+            Pub3 p = new Pub3();
+            p.OnChange += (sender, e) => { Console.WriteLine("Evend handler - Event raised :{0}", e.Value); };
+            p.OnChange += (sender, e) => { Console.WriteLine("Evend handler - Event raised :{0}", e.Value); };
+            p.OnChange += (sender, e) => { Console.WriteLine("Evend handler - Event raised :{0}", e.Value); };
+            p.Raise();
+        }
+    }
+
+    /*
+        Pub class use MyArgs as type of event argument. So when you raising the event, you need to pass an instance of MyArgs. Subscrabers to event can access the arguments and use it
+        You can customise the removel of subscribers which is called a custom event accessor we will see in a next example.
+
+        Custom event accessor looks a lot like proprty with a get and set accessors. Instead of get and set, we use add and remove.
+        Most important one we need to do is making sure this is thread safe via lock.
+        If you use regular event syntax, compiler generates the accessor for you, this makets it clear that events are not delegates, instead they are convenient wrapper around delegatest.
+
+        Delegates executed in sequential order. Generally, the order is the order of how they were added. But it is not a rule, it is not specified within CLI
+
+     */
+
+    public class MyArgs2 : EventArgs
+    {
+        public MyArgs2(int value)
+        {
+            Value = value;
+        }
+        public int Value { get; set; }
+    }
+
+    public class Pub4
+    {
+        private event EventHandler<MyArgs2> onChange = delegate { };
+        public event EventHandler<MyArgs2> OnChange
+        {
+            add
+            {
+                lock(onChange)
+                {
+                    onChange += value;
+                }
+            }
+            remove
+            {
+                lock (onChange)
+                {
+                    onChange -= value;
+                }
+            }
+        }
+
+        public void Raise()
+        {
+            onChange(this, new MyArgs2(42));
+        }
+    }
+    class UsingEventHandler2
+    {
+        public void RunExample()
+        {
+            Pub4 p = new Pub4();
+            p.OnChange += (sender, e) => { Console.WriteLine("Evend handler - Event raised :{0}", e.Value); };
+            p.OnChange += (sender, e) => { Console.WriteLine("Evend handler - Event raised :{0}", e.Value); };
+            p.OnChange += (sender, e) => { Console.WriteLine("Evend handler - Event raised :{0}", e.Value); };
+            p.Raise();
+        }
+    }
+
+
+    public class Pub5
+    {
+        public event EventHandler OnChange = delegate { };
+       
+        public void Raise()
+        {
+            OnChange(this, EventArgs.Empty);
+        }
+    }
+    class ExceptionAtEventHandlerExample
+    {
+        public void RunExample()
+        {
+            Pub5 p = new Pub5();
+
+            p.OnChange += (sender, e) => { Console.WriteLine("Subscriber 1 called"); };
+            p.OnChange += (sender, e) => { throw new Exception("Subscriber 1 called"); };
+            p.OnChange += (sender, e) => { Console.WriteLine("Subscriber 3 called"); };
+
+            try
+            {
+                p.Raise();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Catched : " + ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+
+    /*
+        Normally that was not the original code in the book. I added try catch around p.Raise. But next example will put better exception handling I assume
+        Becase we can not see subscriber 3 called in this base handling so we need better one
+     */
+    public class Pub6
+    {
+        public event EventHandler OnChange = delegate { };
+
+        public void Raise()
+        {
+            var exceptions = new List<Exception>();
+            foreach(Delegate handler in OnChange.GetInvocationList())
+            {
+                try
+                {
+                    handler.DynamicInvoke(this, EventArgs.Empty);
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+            if(exceptions.Count > 0)
+            {
+                throw new Exception($"{exceptions.Count} exception happened");
+            }
+        }
+    }
+
+    class ExceptionAtEventHandlerExample2
+    {
+        public void RunExample()
+        {
+            Pub6 p = new Pub6();
+
+            p.OnChange += (sender, e) => { Console.WriteLine("Subscriber 1 called"); };
+            p.OnChange += (sender, e) => { throw new Exception("Subscriber 2 called"); };
+            p.OnChange += (sender, e) => { Console.WriteLine("Subscriber 3 called"); };
+
+            try
+            {
+                p.Raise();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Catched : " + ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+
 }
